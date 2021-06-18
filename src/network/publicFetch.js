de73059687eb858ab7632a6922eb3e5b531e69d9
bigyank <this.is.blueorange@gmail.com>
2021-06-18 14:34:58 +0545

    removed firebase

--- src/network/publicFetch.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const BASE_PATH = "http://localhost:4000/api";
-
-export const publicFetch = axios.create({
-  baseURL: BASE_PATH,
-});
--- src/utils/humanizetime.js
+++
@@ -1,4 +0,0 @@
-export const timeAgo = (dateString) => {
-  const options = { year: "numeric", month: "long", day: "numeric" };
-  return new Date(dateString).toLocaleDateString(undefined, options);
-};
